6981de6f110552f67550ebc0956c8601c22b18af
fujisawatk <fuji19880707@gmail.com>
2020-05-25 10:20:37 +0900

    add user registration function

--- api/controllers/users_controller.go
+++ api/controllers/users_controller.go
@@ -1,11 +1,40 @@
 package controllers
 
 import (
-	"log"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"merpochi_server/api/database"
+	"merpochi_server/api/models"
 	"net/http"
 )
 
 // CreateUser ユーザー新規登録
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	log.Print("hello public!")
+	// リクエスト情報を読み込む
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// モデルの初期化
+	user := models.User{}
+	// モデルに保管
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// データベースに接続（Gorm使用）
+	db, err := database.Connect()
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	// データベースに保存
+	err = db.Debug().Model(&models.User{}).Create(&user).Error
+	if err != nil {
+		fmt.Println(err)
+	}
+	// 保存後の処理
+	fmt.Println("成功")
 }
