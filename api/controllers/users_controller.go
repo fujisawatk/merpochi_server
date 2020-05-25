6f794fd239698bf0f2916b0ebd3673d59c79c5eb
fujisawatk <fuji19880707@gmail.com>
2020-05-25 13:08:47 +0900

    add error response

--- api/controllers/users_controller.go
+++ api/controllers/users_controller.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"merpochi_server/api/database"
 	"merpochi_server/api/models"
@@ -12,31 +11,31 @@ import (
 
 // CreateUser ユーザー新規登録
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	// リクエスト情報を読み込む
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		fmt.Println(err)
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
 	}
-	// モデルの初期化
+
 	user := models.User{}
-	// モデルに保管
+
 	err = json.Unmarshal(body, &user)
 	if err != nil {
-		fmt.Println(err)
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
 	}
-	// データベースに接続（Gorm使用）
+
 	db, err := database.Connect()
 	if err != nil {
-		fmt.Println(err)
+		responses.ERROR(w, http.StatusInternalServerError, err)
+		return
 	}
 	defer db.Close()
 
-	// データベースに保存
 	err = db.Debug().Model(&models.User{}).Create(&user).Error
 	if err != nil {
-		fmt.Println(err)
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
 	}
 	responses.JSON(w, http.StatusCreated, user)
-	// 保存後の処理
-	fmt.Println("成功")
 }
