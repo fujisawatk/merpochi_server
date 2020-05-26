e4df9c95633cd0530ccb250f652a5cae2364d18d
fujisawatk <fuji19880707@gmail.com>
2020-05-26 18:01:47 +0900

    add auth function

--- interfaces/security/password.go
+++ interfaces/security/password.go
@@ -6,3 +6,8 @@ import "golang.org/x/crypto/bcrypt"
 func Hash(password string) ([]byte, error) {
 	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 }
+
+// VerifyPassword パスワード一致してるか確認
+func VerifyPassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
