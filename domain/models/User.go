b44b73b3514516949632c8cf962757a0a49aee6d
fujisawatk <fuji19880707@gmail.com>
2020-05-26 09:23:17 +0900

    add json display
    レスポンスボティのキー表示変更。

--- domain/models/User.go
+++ domain/models/User.go
@@ -4,10 +4,10 @@ import "time"
 
 // User ユーザー値の保管
 type User struct {
-	ID        uint32
-	Nickname  string
-	Email     string
-	Password  string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        uint32    `json:"id"`
+	Nickname  string    `json:"nickname"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
