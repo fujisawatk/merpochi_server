008ce75e1b128c5d400313f8c90032049fea534b
fujisawatk <fuji19880707@gmail.com>
2020-05-26 09:20:21 +0900

    add function find all users

--- interfaces/controllers/users_controller.go
+++ interfaces/controllers/users_controller.go
@@ -11,6 +11,27 @@ import (
 	"net/http"
 )
 
+// GetUsers 全てのユーザー情報を取得
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	db, err := database.Connect()
+	if err != nil {
+		responses.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	repo := persistence.NewUserPersistence(db)
+
+	func(usersRepository repository.UserRepository) {
+		users, err := usersRepository.FindAll()
+		if err != nil {
+			responses.ERROR(w, http.StatusUnprocessableEntity, err)
+			return
+		}
+		responses.JSON(w, http.StatusOK, users)
+	}(repo)
+}
+
 // CreateUser ユーザー新規登録
 func CreateUser(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
