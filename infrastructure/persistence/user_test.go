4da26c1bc7220f09dfa459eb799e4e498cffa608
fujisawatk <fuji19880707@gmail.com>
2020-06-01 23:18:25 +0900

    add test user find all function

--- infrastructure/persistence/user_test.go
+++ infrastructure/persistence/user_test.go
@@ -33,11 +33,50 @@ func TestSave(t *testing.T) {
 			up := NewUserPersistence(db)
 			got, err := up.Save(tt.args)
 			if (err != nil) != tt.wantErr {
+				// 予期しないエラーの場合
 				t.Errorf("userPersistence.Save() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
+			// 返り値が期待しない値の場合
 			if !reflect.DeepEqual(got.Email, tt.want.Email) {
 				t.Errorf("userPersistence.Save() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFindAll(t *testing.T) {
+	tests := []struct {
+		name    string
+		want    []models.User
+		wantErr bool
+	}{
+		{
+			name: "ユーザー情報を全件取得出来ること",
+			want: []models.User{
+				{
+					Email: "miku@email.com",
+				},
+				{
+					Email: "fuji@email.com",
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			up := NewUserPersistence(db)
+			got, err := up.FindAll()
+			// 予期しないエラーの場合
+			if (err != nil) != tt.wantErr {
+				t.Errorf("userPersistence.FindAll() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			// 返り値が期待しない値の場合
+			if (got[0].Email != tt.want[0].Email) && (got[1].Email != tt.want[1].Email) {
+				t.Errorf("userPersistence.FindAll() → %v, %v want → %v, %v",
+					got[0].Email, got[1].Email, tt.want[0].Email, tt.want[1].Email)
 			}
 		})
 	}
