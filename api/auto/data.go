9f80a06ec820e9f90f581aec726ba1debf7d613d
fujisawatk <fuji19880707@gmail.com>
2020-06-12 00:21:37 +0900

    comment function test
    デバッグ用にインデントしてconsole出力する関数を追加。

--- api/auto/data.go
+++ api/auto/data.go
@@ -4,6 +4,7 @@ import "merpochi_server/domain/models"
 
 var users = []models.User{
 	{
+		ID:       1,
 		Nickname: "miku",
 		Email:    "miku@email.com",
 		Password: "mikumiku",
@@ -12,13 +13,37 @@ var users = []models.User{
 
 var shops = []models.Shop{
 	{
+		ID:   1,
 		Code: "a00000",
+	},
+	{
+		ID:   2,
+		Code: "b11111",
 	},
 }
 
 var favorites = []models.Favorite{
 	{
+		ID:     1,
 		UserID: 1,
+		ShopID: 1,
+	},
+}
+
+var comments = []models.Comment{
+	{
+		ID:     1,
+		Text:   "コメントテストa00000",
+		ShopID: 1,
+	},
+	{
+		ID:     2,
+		Text:   "コメントテストb11111",
+		ShopID: 2,
+	},
+	{
+		ID:     3,
+		Text:   "コメントテストa00000-2",
 		ShopID: 1,
 	},
 }
--- api/auto/load.go
+++ api/auto/load.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"merpochi_server/domain/models"
 	"merpochi_server/infrastructure/database"
+	"merpochi_server/util/console"
 	"merpochi_server/util/security"
 )
 
@@ -17,16 +18,18 @@ func TestLoad() {
 	defer db.Close()
 
 	// テーブルリセット
+	err = db.Debug().Delete(&comments).Error
+	if err != nil {
+		log.Fatal(err)
+	}
 	err = db.Debug().Delete(&users).Error
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	err = db.Debug().Delete(&shops).Error
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	err = db.Debug().Delete(&favorites).Error
 	if err != nil {
 		log.Fatal(err)
@@ -63,11 +66,39 @@ func TestLoad() {
 			log.Fatal(err)
 		}
 		// 指定した店舗IDのレコード数を取得
-		var count uint32
-		err = db.Debug().Model(&models.Favorite{}).Where("shop_id = ?", favorite.ShopID).Count(&count).Error
+		// var count uint32
+		// err = db.Debug().Model(&models.Favorite{}).Where("shop_id = ?", favorite.ShopID).Count(&count).Error
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+	}
+
+	for _, comment := range comments {
+		// 店舗情報を保存
+		err = db.Debug().Model(&models.Comment{}).Create(&comment).Error
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Println(count)
+	}
+	// 指定した店舗IDのコメントを取得
+	// shopComments := db.Debug().Model(&models.Comment{}).Where("shop_id = ?", 2).Find(&comments)
+	// console.Pretty(shopComments)
+
+	// テーブル結合で指定した店舗IDのコメントを取得
+	type Results struct {
+		ID   uint32
+		Code string
+		Text string
+	}
+
+	var results []Results
+	query := db.Debug().Table("shops").
+		Select("shops.id, shops.code, comments.text").
+		Joins("left join comments on comments.shop_id = shops.id").
+		Where("shops.code = ?", "a00000")
+	query.Scan(&results)
+
+	for _, result := range results {
+		console.Pretty(result.Text)
 	}
 }
