f5816d6f50011ce81c7ea2c7b0637ba3d044d180
fujisawatk <fuji19880707@gmail.com>
2020-07-11 04:03:11 +0900

    change GetShops function
    レスポンス値をshopテーブルのIDも返すように変更。

--- usecase/shop.go
+++ usecase/shop.go
@@ -7,7 +7,7 @@ import (
 
 // ShopUsecase Shopに対するUsecaseのインターフェイス
 type ShopUsecase interface {
-	GetShops([]string) ([]int, error)
+	GetShops([]string) ([]shopResponse, error)
 	CreateShop(string) (models.Shop, error)
 	GetShop(uint32) ([]models.Comment, error)
 }
@@ -23,15 +23,20 @@ func NewShopUsecase(sr repository.ShopRepository) ShopUsecase {
 	}
 }
 
-func (su shopUsecase) GetShops(shopCodes []string) ([]int, error) {
-	var commentsCount []int
+func (su shopUsecase) GetShops(shopCodes []string) ([]shopResponse, error) {
+	var commentsCount []shopResponse
 
 	// 取得した店舗IDを1件ずつ登録されているか確認
 	for _, code := range shopCodes {
+		var res shopResponse
 		shop, err := su.shopRepository.SearchShop(code)
 		// 登録されていない場合
 		if err != nil {
-			commentsCount = append(commentsCount, 0)
+			res = shopResponse{
+				ID:    0,
+				Count: 0,
+			}
+			commentsCount = append(commentsCount, res)
 		} else {
 			// 登録されている場合
 			count, err := su.shopRepository.FindAll(shop.ID)
@@ -39,7 +44,11 @@ func (su shopUsecase) GetShops(shopCodes []string) ([]int, error) {
 			if err != nil {
 				count = 0
 			}
-			commentsCount = append(commentsCount, int(count))
+			res = shopResponse{
+				ID:    shop.ID,
+				Count: int(count),
+			}
+			commentsCount = append(commentsCount, res)
 		}
 	}
 	return commentsCount, nil
@@ -65,4 +74,9 @@ func (su shopUsecase) GetShop(sid uint32) ([]models.Comment, error) {
 		return []models.Comment{}, err
 	}
 	return comment, nil
+}
+
+type shopResponse struct {
+	ID    uint32 `json:"id"`
+	Count int    `json:"count"`
 }
