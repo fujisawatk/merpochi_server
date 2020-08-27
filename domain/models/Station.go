70d7057a05ef07a013ab843cb687f48323de984d
fujisawatk <fuji19880707@gmail.com>
2020-08-27 17:50:16 +0900

    change models #29
    stationsテーブルのカラム追加に伴い、モデルも値追加。

--- domain/models/Station.go
+++ domain/models/Station.go
@@ -7,6 +7,8 @@ type Station struct {
 	ID           uint32    `json:"id"`
 	StationName  string    `json:"station_name"`
 	StationNameK string    `json:"station_name_k"`
+	Prefecture   string    `json:"prefecture"`
+	LineOne      string    `json:"line_one"`
+	LineTwo      string    `json:"line_two"`
 	CreatedAt    time.Time `json:"created_at"`
-	UpdatedAt    time.Time `json:"updated_at"`
 }
