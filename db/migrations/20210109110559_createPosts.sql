d1e5585f02da111c9f8a7dce55b093b309ec15df
fujisawatk <fuji19880707@gmail.com>
2021-01-09 12:32:42 +0900

    add post table and ini data #66

---
+++ db/migrations/20210109110559_createPosts.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+CREATE TABLE posts (
+  id INT(11) PRIMARY KEY AUTO_INCREMENT,
+  text VARCHAR(255) NOT NULL,
+  rating INT(11) NOT NULL,
+  shop_id INT(11) NOT NULL,
+  user_id INT(11) NOT NULL,
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE posts;
